4d6be4c533cd34cd1527cb3c4ea8b2cac9ff3702
PardeepKumar816 <pardeepmalhi816@gmail.com>
2024-10-02 01:54:02 +0500

    drawer

--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -7,26 +7,74 @@ class CustomDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      width: 160,
+      backgroundColor: const Color(0xffFBCCA9),
+      width: 250,
       child: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: routes
-              .map(
-                (e) => TextButton(
-                  onPressed: () {},
-                  child: Text(
-                    e,
-                    style: const TextStyle(
-                      fontFamily: 'Montserrat',
-                      fontSize: 16,
-                    ),
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          const SizedBox(
+            height: 32,
+          ),
+          const Center(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "< ",
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Color(0xffB96220),
                   ),
+                ),
+                Text(
+                  "Pardeep",
+                  style: TextStyle(
+                    fontFamily: 'Agustina',
+                    fontSize: 24,
+                    fontWeight: FontWeight.w400,
+                    color: Color(0xffB96220),
+                  ),
+                ),
+                Text(
+                  " >",
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Color(0xffB96220),
+                  ),
+                )
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 24,
+          ),
+          Container(
+            width: double.infinity,
+            height: 1,
+            color: const Color(0xffB96220).withOpacity(0.3),
+          ),
+          const SizedBox(
+            height: 24,
+          ),
+          ...routes
+              .map(
+                (e) => Row(
+                  children: [
+                    TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        e,
+                        style: const TextStyle(
+                          fontFamily: 'Montserrat',
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               )
               .toList(),
-        ),
+        ]),
       ),
     );
   }
--- lib/widgets/social_container.dart
+++ lib/widgets/social_container.dart
@@ -1,25 +1,28 @@
-
 import 'package:flutter/material.dart';
 
 import '../utils/device_size.dart';
 
 class SocialContainer extends StatelessWidget {
   const SocialContainer({
-    Key? key,required this.link,
+    Key? key,
+    required this.link,
   }) : super(key: key);
 
   final String link;
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.only(right: getDeviceSize(context).width*0.012),
-      width: getDeviceSize(context).width < 329 ? 32 :48,
+      margin: EdgeInsets.only(right: getDeviceSize(context).width * 0.012),
+      width: getDeviceSize(context).width < 329 ? 32 : 48,
       height: 48,
-      decoration:  BoxDecoration(
+      decoration: BoxDecoration(
         shape: BoxShape.circle,
-        color: Colors.grey.withOpacity(0.3),
+        color: const Color(0xffB96220).withOpacity(0.3),
       ),
-      child: Image.asset(link,scale: getDeviceSize(context).width < 329 ? 26 : 17,),
+      child: Image.asset(
+        link,
+        scale: getDeviceSize(context).width < 329 ? 26 : 17,
+      ),
     );
   }
 }

8bf83477639f58c2ece632ff51a1531ee593bade
Pardeep Kumar <pardeep.kumar@eplanetglobal.com>
2024-10-03 13:14:15 +0500

    updation

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/pardeepkumar/fvm/versions/3.22.1"
-export "FLUTTER_APPLICATION_PATH=/Users/pardeepkumar/Desktop/flutter projects/protfolio-master"
+export "FLUTTER_ROOT=/Users/pardeepkumar/development/flutter22"
+export "FLUTTER_APPLICATION_PATH=/Users/pardeepkumar/Desktop/flutter/portfolio"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/pardeepkumar/Desktop/flutter/portfolio/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9mNjM0NGI3NWRjZjg2MWQ4YmYxZjEzMjI3ODBiODgxMWY5ODJlMzFhLw=="
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=.dart_tool/package_config.json"
+export "PACKAGE_CONFIG=/Users/pardeepkumar/Desktop/flutter/portfolio/.dart_tool/package_config.json"
